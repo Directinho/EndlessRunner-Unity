9b1826601dbc9707e7f1a59a41927fead909d6d3
DirectDev <jcmassoco2@gmail.com>
2025-10-01 08:17:52 -0300

    Jogo Script fundo e parallax

--- Assets/1. Endless Runner/Scripts/PlayerController.cs
+++ Assets/1. Endless Runner/Scripts/PlayerController.cs
@@ -6,19 +6,22 @@ public class PlayerController : MonoBehaviour
     public float velocidade = 3;
     private Vector3 posicaoInicial;
     Rigidbody2D rb;
-    
-    //método Awake é chamado toda vez que o objeto é criado
-    private void Awake(){
+
+    // Método Awake é chamado toda vez que o objeto é criado
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
         posicaoInicial = transform.position;
     }
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-           Impulsionar();
+            Impulsionar();
         }
     }
+
     private void Impulsionar()
     {
         rb.linearVelocity = Vector2.zero;
